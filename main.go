8d289d2ad00fde1bc8f27a48ee32d76d4056a592
dev-asib <mail.asib.com@gmail.com>
2025-09-06 21:09:24 +0600

    Basic Data Types and If else, Switch Case

--- main.go
+++ main.go
@@ -3,5 +3,54 @@ package main
 import "fmt"
 
 func main() {
+
+	// First Program
 	fmt.Println("Hi! Go")
+
+	// Variable Delclaration
+	var name = "Asib"
+	fmt.Println(name)
+
+	var age int = 25
+	fmt.Println(age)
+
+	country := "Bangladesh"
+	fmt.Println(country)
+
+	// Data Types
+	var mobileBrand string = "Tecno"
+	fmt.Println(mobileBrand)
+
+	var price int = 2500
+	fmt.Println(price)
+
+	var height float32 = 6.7
+	fmt.Println(height)
+
+	var isTecno = true
+	fmt.Println(isTecno)
+
+	taka := 200
+
+	if taka == 20 {
+		fmt.Println("You will get 2 Potatos")
+	} else if taka == 200 {
+		fmt.Println("You will get 1 Potatos")
+	} else if taka <= 0 {
+		fmt.Print("You have no money")
+	} else {
+		fmt.Println("You have balance")
+	}
+
+	userName := "Tamim"
+
+	switch userName {
+	case "Asib":
+		fmt.Print("You are a right user.")
+	case "Samy", "Sabbir":
+		fmt.Println("You are a Asib's Bhai")
+	default:
+		fmt.Println("You are not a valid person")
+	}
+
 }
