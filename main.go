44278c9a5a8f47ba4a97d78853985689449b15fa
dev-asib <mail.asib.com@gmail.com>
2025-09-07 15:31:12 +0600

    Basic Scope

--- main.go
+++ main.go
@@ -2,142 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+var a = 10
+var b = 20
 
-	// First Program
-	fmt.Println("Hi! Go")
-
-	// Variable Delclaration
-	var name = "Asib"
-	fmt.Println(name)
-
-	var age int = 25
-	fmt.Println(age)
-
-	country := "Bangladesh"
-	fmt.Println(country)
-
-	// Data Types
-	var mobileBrand string = "Tecno"
-	fmt.Println(mobileBrand)
-
-	var price int = 2500
-	fmt.Println(price)
-
-	var height float32 = 6.7
-	fmt.Println(height)
-
-	var isTecno = true
-	fmt.Println(isTecno)
-
-	taka := 200
-
-	if taka == 20 {
-		fmt.Println("You will get 2 Potatos")
-	} else if taka == 200 {
-		fmt.Println("You will get 1 Potatos")
-	} else if taka <= 0 {
-		fmt.Print("You have no money")
-	} else {
-		fmt.Println("You have balance")
-	}
-
-	userName := "Tamim"
-
-	switch userName {
-	case "Asib":
-		fmt.Print("You are a right user.")
-	case "Samy", "Sabbir":
-		fmt.Println("You are a Asib's Bhai")
-	default:
-		fmt.Println("You are not a valid person")
-	}
-
-	a := 2
-	b := 5
-
-	c := 10
-	d := 20
-
-	addTwoNumbers(a, b)
-	addTwoNumbers(10, 20)
-	goDeveloperName()
-	sum := addNumb(a, b)
-	fmt.Print(sum)
-	mul, info := multipleReturn(c, d)
-
-	fmt.Println(mul)
-	fmt.Println(info)
-	welcomeCourse("Asib")
-	friends("Samy, ", "Sabbir")
-
-	/// User input
-
-	var myName string
-
-	fmt.Scanln(&myName)
-
-	fmt.Println("My name is - ", myName)
-
-	var myAge int = 24
-	fmt.Println(myAge)
-	fmt.Scanln(&myAge)
-	fmt.Println("My Age is ", myAge)
-
-	/// Simple Application
-
-	/// Welcome message
-	fmt.Println("Welcome to our applicatoin")
-	var uName string
-	fmt.Println("Enter your name - ")
-	fmt.Scanln(&uName)
-
-	var numb1 int
-	var numb2 int
-
-	fmt.Println("Enter num 1 - ")
-	fmt.Scanln(&numb1)
-
-	fmt.Println("Enter numb 2 - ")
-	fmt.Scanln(&numb2)
-
-	fmt.Println("Hi, ", uName)
-
-	summation := numb1 + numb2
-
-	fmt.Println("Summation = ", summation)
-
-	fmt.Println("Thanks for using my application")
-	fmt.Println("Good Bye")
-
-}
-
-func addTwoNumbers(numb1 int, numb2 int) {
-	sum := numb1 + numb2
-
+func add(x int, y int) {
+	sum := x + y
 	fmt.Println(sum)
 }
 
-func goDeveloperName() {
-	fmt.Println("Asib")
+func addTwoNumbersx(x int, y int) {
+	sum := a + b
+	fmt.Println(sum)
 }
 
-/// Single Return Type Function
+func main() {
 
-func addNumb(a int, b int) int {
-	return a + b
-}
+	fmt.Println("Hi, Go")
 
-/// Multiple Return Type Function
+	c := 20
+	d := 30
 
-func multipleReturn(n1 int, n2 int) (int, string) {
-	return n1 * n2, "Asib"
-}
+	add(c, d)
 
-func welcomeCourse(name string) {
-	fmt.Println("Welcome to the Golang course", name, "Bhai")
-}
+	addTwoNumbersx(c, d)
 
-func friends(name1 string, name2 string) {
-	fmt.Print("Our friends: ", name1, name2)
+	var name = "Asib"
+
+	fmt.Println(name)
+
+	m := b
+	fmt.Println(m)
 }
