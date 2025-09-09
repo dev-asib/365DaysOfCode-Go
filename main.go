e4550671176fd6cc8fc89e6b306189949d71893f
dev-asib <mail.asib.com@gmail.com>
2025-09-09 19:43:29 +0600

    Updated

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import "fmt"
 
-var a int = 20
+func add(a, b int) {
+	fmt.Println(a + b)
+}
 
 func main() {
-
-	age := 47
-
-	if age > 18 {
-		a := 30
-		fmt.Println(a)
-	}
-
-	fmt.Println(a)
-
+	add(1, 2)
 }
